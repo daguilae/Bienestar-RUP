db556485d5879492c27af7a6c75b1268fdfd14c8
Gus657 <ggusperezg@gmail.com>
2020-05-25 12:15:39 -0600

    MDI-Maximizado

--- ERP/ERP-RUP/ERP-RUP/ERP-RUP.Designer.cs
+++ ERP/ERP-RUP/ERP-RUP/ERP-RUP.Designer.cs
@@ -29,16 +29,14 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ERP_RUP));
 			this.menuStrip = new System.Windows.Forms.MenuStrip();
-			this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.hRMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.fRMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.cONTABILIDADToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.bANCOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.sCMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.mRPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.seguridadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
 			this.menuStrip.SuspendLayout();
 			this.SuspendLayout();
@@ -46,24 +44,15 @@
 			// menuStrip
 			// 
 			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.inicioToolStripMenuItem,
             this.hRMToolStripMenuItem,
             this.fRMToolStripMenuItem,
             this.sCMToolStripMenuItem,
-            this.mRPToolStripMenuItem,
-            this.seguridadToolStripMenuItem,
-            this.ayudaToolStripMenuItem});
+            this.mRPToolStripMenuItem});
 			this.menuStrip.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip.Name = "menuStrip";
 			this.menuStrip.Size = new System.Drawing.Size(632, 24);
 			this.menuStrip.TabIndex = 0;
 			this.menuStrip.Text = "MenuStrip";
-			// 
-			// inicioToolStripMenuItem
-			// 
-			this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
-			this.inicioToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
-			this.inicioToolStripMenuItem.Text = "Inicio";
 			// 
 			// hRMToolStripMenuItem
 			// 
@@ -110,29 +99,21 @@
 			this.mRPToolStripMenuItem.Text = "MRP";
 			this.mRPToolStripMenuItem.Click += new System.EventHandler(this.MRPToolStripMenuItem_Click);
 			// 
-			// seguridadToolStripMenuItem
-			// 
-			this.seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
-			this.seguridadToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
-			this.seguridadToolStripMenuItem.Text = "Seguridad";
-			// 
-			// ayudaToolStripMenuItem
-			// 
-			this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-			this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
-			this.ayudaToolStripMenuItem.Text = "Ayuda";
-			// 
 			// ERP_RUP
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::ERP_RUP.Properties.Resources.LOGO_BIENESTAR;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
 			this.ClientSize = new System.Drawing.Size(632, 453);
 			this.Controls.Add(this.menuStrip);
+			this.DoubleBuffered = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.IsMdiContainer = true;
 			this.MainMenuStrip = this.menuStrip;
 			this.Name = "ERP_RUP";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "ERP_RUP";
+			this.Text = "ERP BIENESTAR";
 			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
 			this.Load += new System.EventHandler(this.ERP_RUP_Load);
 			this.menuStrip.ResumeLayout(false);
@@ -146,13 +127,10 @@
 
 		private System.Windows.Forms.MenuStrip menuStrip;
 		private System.Windows.Forms.ToolTip toolTip;
-		private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem hRMToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem mRPToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem sCMToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem fRMToolStripMenuItem;
-		private System.Windows.Forms.ToolStripMenuItem seguridadToolStripMenuItem;
-		private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem cONTABILIDADToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem bANCOSToolStripMenuItem;
 	}
